3a9cddf184d37e4d2a45174373099d0c2bb59a0a
Aaron Buffie <abuffie@adxnet.com>
2023-02-21 13:41:38 -0600

    clean up some spacing added reincatnation effect

--- Runtime/Enums/Effect.cs
+++ Runtime/Enums/Effect.cs
@@ -3,17 +3,18 @@ namespace Pixelakes.Wrath.Enums {
 		None				= 0, //No Effect
 		Heal				= 100, //Give Power Points to card
 		Immune				= 101, //Cannot be damaged or targetted
-		Resurrect				= 102, //Move card back to (hand or deck) from graveyard
+		Resurrect			= 102, //Move card back to (hand or deck) from graveyard
+		Reincarnation 		= 103, //Come back to life on death with init state
 		Damage				= 200, //Take Power Points from a card
 		Destroy				= 201, //Send card to the graveyard
-		Retaliate				= 202, //Damages the attacker by the same amount, if the attacker is not a unit, player may select the target
+		Retaliate			= 202, //Damages the attacker by the same amount, if the attacker is not a unit, player may select the target
 		Clone				= 300, //Copies card in play and depolys it immediately skipping OnDeploy triggers
 		Spawn				= 301, //spwans weak generic card
 		Summon				= 302, //Deploys all cards in hand matching the calling card
 		Lock				= 400, //Renders card usless and removes its points from the count
 		Unlock				= 401, //Removes the lock on a locked card
-		Teleport				= 402, //Moves card to a new row
-		Protection				= 403, //Bubble protection absorbs next damage
+		Teleport			= 402, //Moves card to a new row
+		Protection			= 403, //Bubble protection absorbs next damage
 		Silence				= 404, //Removes any status effects
 		Reveal				= 500, //Shows Card to other player
 		Shuffle				= 501, //Shuffle the deck
@@ -24,6 +25,6 @@ namespace Pixelakes.Wrath.Enums {
 		Stealth				= 601, //Cannot be targetted
 		Consume				= 700, //Consumes power from other cards destroying them
 		Absorb				= 701, //Absorbs damage on other cards up current power destroying self
-		Displace				= 702, //Destributes its power as damage to foes or power to friends and destroys self
+		Displace			= 702, //Destributes its power as damage to foes or power to friends and destroys self
    }
 }
