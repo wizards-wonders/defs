bc170bb08d7421ba4cdf5fb5c585bd4e489ae56a
Aaron Buffie <abuffie@adxnet.com>
2023-02-24 13:04:02 -0600

    added the entities enum

---
+++ Runtime/Enums/TargetEntity.cs
@@ -0,0 +1,10 @@
+namespace Pixelakes.Wrath.Enums {
+	public enum TargetEntity{
+		None				= 0, //No Target
+		Card				= 100, //The card using the ability
+		Player				= 200, //Player
+		Opponent				= 300, //Opponent
+		Random				= 400, //Computer picks Player or Opponent
+		All				= 500, //All Players or  All Players of (Target)
+   }
+}
