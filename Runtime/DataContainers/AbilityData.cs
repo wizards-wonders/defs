d4416e99d8d2343e159449e783049c5f622a9c04
Aaron Buffie <abuffie@adxnet.com>
2023-03-06 10:48:20 -0600

    updated prop name for action entity

--- Runtime/DataContainers/AbilityData.cs
+++ Runtime/DataContainers/AbilityData.cs
@@ -36,15 +36,22 @@ namespace Pixelakes.Wrath{
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        triggerEffect;          // The action (Effect) that invokes uses if Trigger is Effect    
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        action;                 // The action (Effect) ran
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        offering;               // The offering (Effect) ran, Sacerfice, Cost for use
+        
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Target        actionTarget;           // Target card(s) for the action (Effect) to be applied to
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Target        offeringTarget;         // Target card(s) for the offering (Effect)  to be applied to
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Target        triggerTarget;         // Target card(s) for the Trigger (Effect)  to be applied to
+        
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        actionEffectTarget;     // Target card(s) with Effect for the action (Effect) to be applied to
-        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        offeringEffectTarget;   // Target card(s) with Effect for the offering (Effect)  to be applied to         
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        offeringEffectTarget;   // Target card(s) with Effect for the offering (Effect)  to be applied to 
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public Effect        triggerEffectTarget;   // Target card(s) with Effect for the Trigger (Effect)  to be applied to         
+        
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public SubType       actionSubTypeTarget;    // Card SubType Action Targets if any     
-        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public SubType       offeringSubTypeTarget;  // Card SubType Offering Targets if any
-        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public TargetEntity  targetEntity;           // (Card, Player, Opponent, Random, etc) of the Target        
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public SubType       offeringSubTypeTarget;  // Card SubType Offering Targets if any   
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public SubType       triggerSubTypeTarget;  // Card SubType Trigger Targets if any
+        
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public TargetEntity  actionEntity;           // (Card, Player, Opponent, Random, etc) of the Target        
         [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public TargetEntity  offeringEntity;         // (Card, Player, Opponent, Random, etc) of the offering Target
-        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public TargetEntity  triggerEntity;          // (Card, Player, Opponent, Random, etc) that invokes the trigger        
+        [JsonProperty, JsonConverter(typeof(StringEnumConverter))] public TargetEntity  triggerEntity = TargetEntity.Card;          // (Card, Player, Opponent, Random, etc) that invokes the trigger        
         
 
 
@@ -54,20 +61,29 @@ namespace Pixelakes.Wrath{
         public string       Name                    => name;
         public string       Description             => description;
         public AbilityType  Type                    => type;                    // if trigger is auto or user fired
-        public Trigger      Trigger                 => trigger;                 // what triggers the action        
+
+        public Trigger      Trigger                 => trigger;                 // what triggers the action   
         public Effect       TriggerEffect           => triggerEffect;           // what effect triggers the action if trigger is effect
+
         public Effect       Action                  => action;                  // type of action
         public Effect       Offering                => offering;                // type of offering - i.e cost paid or sacrifice made
+        
         public Target       ActionTarget            => actionTarget;            // who the action affects
         public Target       OfferingTarget          => offeringTarget;          // who the offering affects
+        public Target       TriggerTarget           => triggerTarget;           // who/what triggers the effects
+        
         public Effect       ActionEffectTarget      => actionEffectTarget;      // Effect on Cards that should be Targeted for the action (Effect) to be applied to
         public Effect       OfferingEffectTarget    => offeringEffectTarget;    // Effect on Cards that should be Targeted for the offering (Effect)  to be applied to         
+        public Effect       TriggerEffectTarget     => triggerEffectTarget;    // Effect on Cards that should be Targeted for the offering (Effect)  to be applied to         
        
         public SubType      ActionSubTypeTarget     => actionSubTypeTarget;     // SubType the action affects if set
         public SubType      OfferingSubTypeTarget   => offeringSubTypeTarget;   // SubType the offering affects if set
-        public TargetEntity TargetEntity            => targetEntity;            // (Card, Player, Opponent, Random, etc) of the Target
+        public SubType      TriggerSubTypeTarget    => triggerSubTypeTarget;    // SubType the offering affects if set
+        
+        public TargetEntity ActionEntity            => actionEntity;            // (Card, Player, Opponent, Random, etc) of the Target
         public TargetEntity OfferingEntity          => offeringEntity;          // (Card, Player, Opponent, Random, etc) of the offering Target
         public TargetEntity TriggerEntity           => triggerEntity;           // (Card, Player, Opponent, Random, etc) That invokes the trigger
+        
         public int          TargetCount             => targetCount;             // point value of action
         public int          OfferingCount           => offeringCount;           // point value of offering
         public int          ActionValue             => actionValue;             // point value of action
