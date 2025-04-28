746a64304ccbdfded78030d2ed40c13da993719e
Aaron Buffie <abuffie@adxnet.com>
2025-04-28 15:07:29 -0500

    trait base

--- Runtime/Enums/Trait.cs
+++ Runtime/Enums/Trait.cs
@@ -1,21 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Pixelakes
-{
-    public class Trait : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+namespace Pixelakes.Wrath.Enums {
+	public enum Trait{
+   }
 }
