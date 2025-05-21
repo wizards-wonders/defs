989461fc1c981b922c1278338560d96741b468fb
Aaron Buffie <abuffie@adxnet.com>
2025-05-21 13:18:07 -0500

    working on array enum converter

--- Runtime/JsonConverters/TraitArrayConverter.cs
+++ Runtime/JsonConverters/TraitArrayConverter.cs
@@ -2,24 +2,23 @@ namespace Pixelakes.Wrath.JsonConverters{
     
     using System;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
     using Pixelakes.Wrath.Enums;
     public class TraitArrayConverter : BaseVectorConverter<string[]>{
        
         protected override string type {get=>"Trait[]";}
        
-/*
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
+
+        public override object ReadJson(JsonReader reader, System.Type objectType, object existingValue, JsonSerializer serializer){
             JToken token = JToken.Load(reader);
-            if (token.Type == JTokenType.Array)
-            {
+            if (token.Type == JTokenType.Array){
                 string[] stringValues = token.ToObject<string[]>();
                 return Array.ConvertAll(stringValues, s => (Trait)Enum.Parse(typeof(Trait), s, true));
             }
             return null;
         }
-*/
+
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer){
             Trait[] traits = value as Trait[];
             if (traits != null){
